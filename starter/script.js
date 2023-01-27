fd05212b649b8cbdada27768ab1205d068b2bdeb
Ope <o_orekoya@yahoo.com>
2023-01-27 21:23:23 +0000

    added element creation

--- starter/script.js
+++ starter/script.js
@@ -2,14 +2,64 @@ apiKey = "e6a7e2d17f01690a1f7665bfa3295ba7"
 
 
 
-let queryUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`
+$("#search-form").on("submit", function(event){
+    event.preventDefault()
 
-$(".#search-form").on("submit", function(){
     let searchInput = $("#search-input").val()
 
-    let countryQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=10&appid=${apiKey}`
+    let countryQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`
 
     $.ajax({url : countryQueryUrl}).then(function(response){
-        console.log(response)
+
+        let lat = response[0].lat
+        let lon = response[0].lon
+        
+        let queryUrl = `http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+
+        $.ajax({url : queryUrl}).then(function(weatherResponse){
+
+                
+
+                const weatherList = weatherResponse.list
+                const weatherToday = weatherList[0]
+                
+                console.log(weatherResponse)
+
+                let weatherTodayEL = $(` <h1> ${weatherResponse.city.name} (${moment(weatherToday.dt_txt, "YYYY-MM-DD HHHH").format("DD/MM/YYYY")}) <img src="${`http://openweathermap.org/img/w/${weatherToday.weather[0].icon}.png`}"  alt="Weather icon"></h1>
+                                         <p> Temp: ${weatherToday.main.temp } &#8451</p>
+                                         <p> Wind: ${weatherToday.wind.speed} KPH </p> 
+                                         <p> Humidity: ${weatherToday.main.humidity}% </p> `)
+                
+                let todayEl = $("#today")
+                todayEl.empty()
+                todayEl.append(weatherTodayEL)
+
+                console.log(weatherToday)
+
+                let forecastEl = $("#forecast")
+                forecastEl.empty()
+
+                for(let i = 0; i < weatherList.length; i += 8){
+                    
+
+                    const weatherForecast = weatherList[i]
+
+                    let weatherForecastEL = $(` <h1> ${weatherResponse.city.name} (${moment(weatherForecast.dt_txt, "YYYY-MM-DD HHHH").format("DD/MM/YYYY")}) <img src="${`http://openweathermap.org/img/w/${weatherForecast.weather[0].icon}.png`}"  alt="Weather icon"></h1>
+                                            <p> Temp: ${weatherForecast.main.temp} &#8451</p>
+                                            <p> Wind: ${weatherForecast.wind.speed} KPH </p> 
+                                            <p> Humidity: ${weatherForecast.main.humidity}% </p> `)
+                    
+                    
+                    forecastEl.append(weatherForecastEL)
+
+                    console.log(weatherForecastEL)
+
+
+                }
+                
+            })
     })
+    
+    
 })
